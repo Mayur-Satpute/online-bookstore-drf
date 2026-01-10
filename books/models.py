830ee1a90fc3e0f9262f5809081b614a9b5697fe
Mayur Satpute <mayursatpute246@gmail.com>
2026-01-11 01:07:12 +0530

    feat(books): add category and book models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+    isbn = models.CharField(max_length=13, unique=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to="books/")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
+
+    def __str__(self):
+        return self.title
+
